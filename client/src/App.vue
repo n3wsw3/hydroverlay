09df6e6cc86631b0f6f43a5182c8c69c956b6b7e
Simon Uggla <simon.uggla@aol.com>
2022-02-05 00:19:18 +0100

    Added overlay

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <div class="overlay">
     <div class="blackish">
       <p>Water: {{ data.waterLevel }} Liter</p>
@@ -8,57 +7,9 @@
       <p>pH: {{ data.ph / 10 }}</p>
     </div>
   </div>
-  <!-- <Particles
-    id="particle"
-    :options="{
-      // autoPlay: true,
-      backgroundMode: {
-        enable: true,
-      },
-      motion: {
-        disable: false,
-      },
-      emitters: {
-        direction: 'top',
-        size: {
-          width: 50,
-          height: 0,
-        },
-        rate: {
-          delay: 5,
-          quantity: 2,
-        },
-        position: {
-          x: 90,
-          y: 90,
-        },
-      },
-      particles: {
-        number: {
-          value: 0,
-        },
-        color: {
-          value: ['#ab058b', '#4c0082'],
-        },
-        size: {
-          value: 50,
-        },
-        move: {
-          enable: true,
-          speed: 4,
-          direction: 'none',
-          random: true,
-          straight: false,
-          out_mode: 'destroy',
-        },
-      },
-    }"
-  >
-  </Particles> -->
 </template>
 
 <script setup lang="ts">
-// import HelloWorld from "./components/HelloWorld.vue";
 import { ref } from "vue";
 import { io } from "socket.io-client";
 const socket = io("localhost:8000");
@@ -72,42 +23,37 @@ interface SensorData {
 
 const data = ref<SensorData>({ waterLevel: 0, temperature: 0, ec: 0, ph: 0 });
 
-socket.on("connection", () => {
-  console.log("tjo");
-  socket.emit("TJO");
-});
-
 socket.on("update", (_data) => {
   data.value = _data;
 });
 </script>
 
 <style lang="less">
+* {
+  box-sizing: border-box;
+}
+
 body {
   padding: 0;
   margin: 0;
-  box-sizing: border-box;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: white;
-  /* margin-top: 60px; */
 }
 
-#particle {
-  height: 200px;
-}
 .overlay {
+  background: no-repeat 0 0 url('~@/assets/overlay.png');
+  background-size: 100% 100%;
+  padding: 70px;
   width: 100vw;
   height: 100vh;
   color: white;
   .blackish {
     margin: 0;
     padding: 10px;
-    // width: 100px;
     display: inline-block;
     background-color: rgba(black, 0.2);
 
